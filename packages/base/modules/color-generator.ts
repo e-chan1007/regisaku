9c1f425f5cd589c2e6df684f82c20026b2078aa4
いーちゃん(e_chan1007) <echan041007@gmail.com>
2025-08-21 00:35:31 +0900

    chore: 最低限のスタイル定義

---
+++ packages/base/modules/color-generator.ts
@@ -0,0 +1,44 @@
+import chroma from "chroma-js";
+import { writeFile } from "fs/promises";
+import { defineNuxtModule } from "nuxt/kit";
+import { resolve } from "path";
+
+const createCSSVariables = (key: string, color: string): string => {
+  const colorPalette = chroma
+    .scale([
+      chroma(color).set("lch.l", 90),
+      chroma(color).set("lch.l", 50),
+      chroma(color).set("lch.l", 20),
+    ])
+    .mode("lch")
+    .colors(10);
+
+  return colorPalette
+    .map((color, index) => `  --color-${key}-${index}: ${color};`)
+    .join("\n");
+};
+
+export default defineNuxtModule({
+  setup(_options, nuxt) {
+    const buildDir = nuxt.options.buildDir;
+    const outputPath = resolve(buildDir, "color-palette.css");
+    nuxt.options.css.unshift(outputPath);
+
+    nuxt.hook("nitro:build:before", async () => {
+      const colors: Record<string, string> =
+        nuxt.options.runtimeConfig.theme?.colors ?? {};
+
+      const css = [
+        ":root {",
+        Object.entries(colors).map(([key, color]) =>
+          createCSSVariables(key, color),
+        ),
+        "}",
+      ]
+        .flat()
+        .join("\n");
+
+      await writeFile(outputPath, css);
+    });
+  },
+});
--- packages/base/nuxt.config.ts
+++ packages/base/nuxt.config.ts
@@ -1,4 +1,50 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import ColorGenerator from "./modules/color-generator";
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
+
+declare module "nuxt/schema" {
+  interface RuntimeConfig {
+    theme?: {
+      colors?: {
+        primary?: string;
+        gray?: string;
+        info?: string;
+        success?: string;
+        error?: string;
+        warning?: string;
+      };
+    };
+  }
+}
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-08-20",
-  modules: ["@nuxt/fonts", "@nuxt/icon", "@nuxt/scripts"],
+  modules: ["@nuxt/fonts", "@nuxt/icon", "@nuxt/scripts", ColorGenerator],
+  css: [join(currentDir, "./app/assets/styles/global.scss")],
+  runtimeConfig: {
+    theme: {
+      colors: {
+        primary: "#0063b2",
+        gray: "#374151",
+        info: "#3b92f6",
+        success: "#22a06b",
+        error: "#b91c3a",
+        warning: "#c58a1c",
+      },
+    },
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "sass:math";
+            @use "${join(currentDir, "./app/assets/styles/variables")}" as *;
+          `,
+        },
+      },
+    },
+  },
 });
